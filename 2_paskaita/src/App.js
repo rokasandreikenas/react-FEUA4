b14a7e3144092cddec228e96e4000869fb011360
rokasandreikenas <shyptt@gmail.com>
2023-05-18 20:30:21 +0300

    Add state management examples in three different ways

--- 2_paskaita/src/App.js
+++ 2_paskaita/src/App.js
@@ -1,5 +1,24 @@
+import Car from "./components/Car";
+import FuncCar from "./components/FuncCar";
+import FuncBestCar from "./components/FuncBestCar";
+
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h2>Class</h2>
+      <Car />
+      <br />
+      <br />
+      <br />
+      <h2>Function with one state</h2>
+      <FuncCar />
+      <br />
+      <br />
+      <br />
+      <h2>Function with separate states</h2>
+      <FuncBestCar />
+    </div>
+  );
 };
 
 export default App;
---
+++ 2_paskaita/src/components/FuncCar.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+// funkciniam komponente nera zodelio this.
+const FuncCar = () => {
+  const [state, setState] = useState({
+    brand: "Ford",
+    model: "Mustang",
+    color: "red",
+    year: 1964,
+  });
+
+  const changeColor = () => {
+    setState({ ...state, color: "blue" });
+  };
+
+  return (
+    <div>
+      <h1>My car {state.brand}</h1>
+      <p>
+        It is a {state.color} {state.model} from {state.year}
+      </p>
+      <button onClick={changeColor}>Change color</button>
+    </div>
+  );
+};
+
+export default FuncCar;
